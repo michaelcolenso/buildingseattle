e4b1965b18adf73c846def880fd203af9f771a5a
Michael Colenso <github@michaelcolenso.com>
2026-04-24 07:15:28 -0700

    Add permit schema migrations

---
+++ migration_permit_import_columns.sql
@@ -0,0 +1,3 @@
+ALTER TABLE permits ADD COLUMN housing_units INTEGER DEFAULT 0;
+ALTER TABLE permits ADD COLUMN applied_date DATE;
+ALTER TABLE permits ADD COLUMN completed_date DATE;
---
+++ migration_permit_people.sql
@@ -0,0 +1,4 @@
+ALTER TABLE permits ADD COLUMN owner_name TEXT;
+ALTER TABLE permits ADD COLUMN owner_address TEXT;
+ALTER TABLE permits ADD COLUMN applicant_name TEXT;
+ALTER TABLE permits ADD COLUMN architect_name TEXT;
